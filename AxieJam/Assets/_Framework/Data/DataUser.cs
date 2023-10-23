14fcf06a48659e21e40cdad548dcdf5e35f1441f
nguyenphancuong <nguyenphancuong097@gmail.com>
2023-10-23 17:18:23 +0700

    + fix button click

--- AxieJam/Assets/_Framework/Data/DataUser.cs
+++ AxieJam/Assets/_Framework/Data/DataUser.cs
@@ -1,8 +1,3 @@
-using Newtonsoft.Json.Linq;
-using Sirenix.OdinInspector;
-using Sirenix.Utilities;
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
